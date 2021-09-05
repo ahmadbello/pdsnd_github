865c17babc144db68ff8f91570c144a4ef56c6e3
ahmadbello <90ahmadbello@gmail.com>
2021-09-05 18:27:03 +0100

    changed variable names to more descriptive names

--- bikeshare.py
+++ bikeshare.py
@@ -52,13 +52,13 @@ def load_data(city, month, day):
     
     # to see 5 lines of raw data
     to_see = input('Would you like to see to see 5 lines of raw data? Enter yes or no:')
-    st = 0
-    sp = 5
+    strt = 0
+    stp = 5
     while to_see == 'yes':
         print(df[st:sp].head())
         to_see = input('Would you like to see to see  another 5 lines of raw data? Enter yes or no:')
-        st += 5
-        sp += 5
+        strt += 5
+        stp += 5
         continue
     
         
